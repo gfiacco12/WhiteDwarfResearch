d0f50dfe09872bc80511898e1a8d1cc4415c1ec5
gfiacco12 <gfiacco12@gmail.com>
2023-08-02 15:10:12 -0600

    added in tides function

--- graces_phd/Frequency Calculations/Calculations.py
+++ graces_phd/Frequency Calculations/Calculations.py
@@ -2,28 +2,68 @@ from numpy import number
 import numpy as np
 from const import *
 
-def Frequency_1PN(freq0, chirpMass, totalMass, t_obs):
+def Frequency_1PN(freq0, mass1, mass2, t_obs):
 
+    chirpMass = (mass1*mass2)**(3/5) / (mass1 + mass2)**(1/5)
+    totalMass = mass1 + mass2
     eta = (chirpMass/totalMass)**(5/3)
     #0PN point particle freqD
     fdot_pp = 96/5*np.pi**(8/3)*freq0**(11/3)*chirpMass**(5/3)
-    
+    fd_corr = ((743/1344)-(11*eta/16))*(8*np.pi*totalMass*freq0)**(2/3)
     #1PN freqD and freqDD
     fdot = fdot_pp * (1 + ((743/1344)-(11*eta/16))*(8*np.pi*totalMass*freq0)**(2/3))
     fddot = fdot_pp * (fdot/freq0) * ((11/3) + (13/3)*((743/1344)-(11*eta/16))*(8*np.pi*totalMass*freq0)**(2/3))
-    fddot_0PN = (fdot ** 2) / freq0
+    fddot_0PN = (11/3)*(fdot ** 2) / freq0
     delta_fddot = fddot - fddot_0PN 
 
     #frequency bin
     df = (1/t_obs)
     df_1 = 3.1e-8 * (1/t_obs)
 
+    print("1PN CORRECTION TERMS")
+    print("-----------------------------------------------")
+    print("eta:", eta)
     print("0PN point particle fdot:", fdot_pp, "Hz")
     print("0PN freqDD:", fddot_0PN, "Hz")
     print("1PN Freq Derivative =", fdot, "Hz")
     print("1PN Freq Second Derivative =", fddot, "Hz")
+    print("1PN fdot corr:", fd_corr)
     print("The 1PN Fdd correction is:", delta_fddot, "Hz")
-    print("Fractional 1PN Fdd:", 1 - delta_fddot/fddot, "Hz")
-    print("-----------------------------------------------")
     print("Frequency bin:", df_1, "Hz")
+    print("-----------------------------------------------")
+    return fdot, fddot, delta_fddot 
+
+
+def Frequency_Tides(freq0, mass1, mass2, t_obs):
+
+    chirpMass = (mass1*mass2)**(3/5) / (mass1 + mass2)**(1/5)
+    totalMass = mass1 + mass2
+    eta = (chirpMass/totalMass)**(5/3)
+    #0PN point particle freqD
+    fdot_pp = 96/5*np.pi**(8/3)*freq0**(11/3)*chirpMass**(5/3)
+    
+    I_wd = 8.51e-10 * ((mass1/0.6*MSOLAR)**(1/3) + (mass2/0.6*MSOLAR)**(1/3))
+    I_orb = chirpMass**(5/3) / ((np.pi*freq0)**(4/3))
+    #1PN freqD and freqDD
+    fdot = fdot_pp * (1 + ((3*I_wd/I_orb)/(1 - (3*I_wd/I_orb))) )
+    fddot = (11/3)*(fdot_pp * fdot/freq0 )* ((1 - (21/11)*(I_wd/I_orb)) / ((1 - (3*I_wd/I_orb))**2))
+    fddot_0PN = (11/3)*(fdot ** 2) / freq0
+    delta_fddot = fddot - fddot_0PN 
+    fd_corr = (3*I_wd/I_orb)/(1 - (3*I_wd/I_orb))
+
+    #frequency bin
+    df = (1/t_obs)
+    df_1 = 3.1e-8 * (1/t_obs)
+
+    print("TIDAL CORRECTION TERMS")
+    print("-----------------------------------------------")
+    print("eta:", eta)
+    print("0PN point particle fdot:", fdot_pp, "Hz")
+    print("0PN freqDD:", fddot_0PN, "Hz")
+    print("Tides Freq Derivative =", fdot, "Hz")
+    print("Tides Freq Second Derivative =", fddot, "Hz")
+    print("Tidal fdot corr:", fd_corr)
+    print("The Tidal Fdd correction is:", delta_fddot, "Hz")
+    print("Frequency bin:", df_1, "Hz")
+    print("-----------------------------------------------")
     return fdot, fddot, delta_fddot 
--- graces_phd/Frequency Calculations/main.py
+++ graces_phd/Frequency Calculations/main.py
@@ -1,11 +1,12 @@
 import numpy as np
-from Calculations import Frequency_1PN
+from Calculations import Frequency_1PN, Frequency_Tides
 from const import *
 
-def main(freq0, chirpMass, totalMass, t_obs):
+def main(freq0, mass1, mass2, t_obs):
     #Calculates frequency derivatives and other relevant quantities for binary white dwarf in LISA
     #INPUTS: Initial frequency (Hz), chirp mass (s), total mass (s)
 
-    Frequency_1PN(freq0, chirpMass, totalMass, t_obs)
+    Frequency_1PN(freq0, mass1, mass2, t_obs)
+    Frequency_Tides(freq0, mass1, mass2, t_obs)
 
-main(10.e-3, 0.522*MSOLAR, 1.4*MSOLAR, 4)
+main(10.e-3, 0.6*MSOLAR, 1.0*MSOLAR, 4)
