cbfe6b3a4f052ed2481ce0e8a5cb0148d5ccea5b
gfiacco12 <gfiacco12@gmail.com>
2023-08-03 17:11:31 -0600

    add in signal and amp calc function

--- graces_phd/Frequency Calculations/Calculations.py
+++ graces_phd/Frequency Calculations/Calculations.py
@@ -2,7 +2,7 @@ from numpy import number
 import numpy as np
 from const import *
 
-def Frequency_1PN(freq0, mass1, mass2, t_obs):
+def Frequency_1PN(freq0, mass1, mass2, dl, t_obs):
 
     chirpMass = (mass1*mass2)**(3/5) / (mass1 + mass2)**(1/5)
     totalMass = mass1 + mass2
@@ -16,9 +16,11 @@ def Frequency_1PN(freq0, mass1, mass2, t_obs):
     fddot_0PN = (11/3)*(fdot ** 2) / freq0
     delta_fddot = fddot - fddot_0PN 
 
+    amp = np.pi**2/3 * chirpMass**(5/3) * freq0**(2/3) / dl
+
     #frequency bin
     df = (1/t_obs)
-    df_1 = 3.1e-8 * (1/t_obs)
+    #df_1 = 3.1e-8 * (1/t_obs)
 
     print("1PN CORRECTION TERMS")
     print("-----------------------------------------------")
@@ -29,12 +31,13 @@ def Frequency_1PN(freq0, mass1, mass2, t_obs):
     print("1PN Freq Second Derivative =", fddot, "Hz")
     print("1PN fdot corr:", fd_corr)
     print("The 1PN Fdd correction is:", delta_fddot, "Hz")
-    print("Frequency bin:", df_1, "Hz")
+    print("Frequency bin:", df, "Hz")
+    print("amplitude:", amp)
     print("-----------------------------------------------")
-    return fdot, fddot, delta_fddot 
+    return fdot, fddot
 
 
-def Frequency_Tides(freq0, mass1, mass2, t_obs):
+def Frequency_Tides(freq0, mass1, mass2, dl, t_obs):
 
     chirpMass = (mass1*mass2)**(3/5) / (mass1 + mass2)**(1/5)
     totalMass = mass1 + mass2
@@ -51,9 +54,12 @@ def Frequency_Tides(freq0, mass1, mass2, t_obs):
     delta_fddot = fddot - fddot_0PN 
     fd_corr = (3*I_wd/I_orb)/(1 - (3*I_wd/I_orb))
 
+    amp = np.pi**2/3 * chirpMass**(5/3) * freq0**(2/3) / dl
+
+
     #frequency bin
     df = (1/t_obs)
-    df_1 = 3.1e-8 * (1/t_obs)
+    #df_1 = 3.1e-8 * (1/t_obs)
 
     print("TIDAL CORRECTION TERMS")
     print("-----------------------------------------------")
@@ -64,35 +70,60 @@ def Frequency_Tides(freq0, mass1, mass2, t_obs):
     print("Tides Freq Second Derivative =", fddot, "Hz")
     print("Tidal fdot corr:", fd_corr)
     print("The Tidal Fdd correction is:", delta_fddot, "Hz")
-    print("Frequency bin:", df_1, "Hz")
+    print("Frequency bin:", df, "Hz")
     print("-----------------------------------------------")
-    return fdot, fddot, delta_fddot 
+    return fdot, fddot
 
 
-def fisherMatrix(t_obs):
-    #define f(t), phi(t)
-    #declare variables for fisher
-    t = np.linspace[0, t_obs, 1000]
+def GWsignal(t_obs, params):
+    #generates simple sinusoidal signal
+    A = params[0]
+    phi0 = params[1]
+    f0 = params[2]
+    fD = params[3]
+    fDD = params[4]
+    #parameterize the signal - now params around order unity
+    alpha = f0 * t_obs
+    beta = fD * t_obs**2
+    gamma = fDD * t_obs**3
+    t = np.linspace(0, t_obs, 1000)
+
+    print("alpha:", alpha) #~ 1.2e6
+    print("beta:", beta) # ~210
+    print('gamma:', gamma) # ~ 0.1
+    phi = phi0 + 2*np.pi*alpha*(t/t_obs) + np.pi*beta*(t/t_obs)**2 + (np.pi/3)*gamma*(t/t_obs)**3
+    h = A * np.cos(2 * np.pi * phi)
+    return h
+
+
+def calculateAmplitude(SNR, t_obs):
+    #calculates approx amplitude from SNR^2 = int( h(t)^2 dt) - time avg over 0 to t_obs
+    return(np.sqrt(2)*SNR / np.sqrt(t_obs))
+
+
+def fisherMatrix(t_obs, A, phi0, f0, fD, fDD):
+    #define parameters
+    params = np.array([A, phi0, f0, fD, fDD])
+    #time series
+    print(A)
+    signal = GWsignal(t_obs, params)
+    #initialize matrix and step size vectors
+    vector1 = np.zeros(np.size(params))
+    vector2 = np.zeros(np.size(params))
+    fisher = np.zeros((np.size(params),np.size(params)))
+    
+    #step sizes
+    amp_step = 1.e-6
+    phi_step = 0.001
+    f0_step = 1.e3
+    fD_step = 0.1
+    fDD_step = 0.001
+
+
+
 
     #create a function to calculate h(t). Use LISA code to grab values for the params. Copy fisher matrix
     #code from my github to do the actual derivative and looping through the values and elements of matrix
     # maybe look into scipy function to take derivatives instead of coding it myself
-
-    phi = lambda phi0, f0, fD, fDD : phi0 + 2*np.pi*f0*t + np.pi*fD*t**2 + (np.pi/3)*fDD*t**3
-    h = lambda A, phi0, f0, fD, fDD : A * np.cos(2 * np.pi * phi(phi0, f0, fD, fDD))
-
     #take derivatives for elements of fisher matrix
     return
-
-def fisherMatrixElement(noise = 1):
-    const = 2 / noise
-    
-    return
-
-def h(t):
-    return
-
-def add(x, y):
-    return x, y
-
-add = lambda x, y : x + y
--- graces_phd/Frequency Calculations/main.py
+++ graces_phd/Frequency Calculations/main.py
@@ -1,12 +1,14 @@
 import numpy as np
-from Calculations import Frequency_1PN, Frequency_Tides
+from Calculations import Frequency_1PN, Frequency_Tides, fisherMatrix, calculateAmplitude
 from const import *
 
-def main(freq0, mass1, mass2, t_obs):
+def main(freq0, mass1, mass2, dl, t_obs):
     #Calculates frequency derivatives and other relevant quantities for binary white dwarf in LISA
     #INPUTS: Initial frequency (Hz), chirp mass (s), total mass (s)
 
-    Frequency_1PN(freq0, mass1, mass2, t_obs)
-    Frequency_Tides(freq0, mass1, mass2, t_obs)
+    fD_1PN, fDD_1PN = Frequency_1PN(freq0, mass1, mass2, dl, t_obs)
+    fD_tide, fDD_tide = Frequency_Tides(freq0, mass1, mass2, dl, t_obs)
 
-main(10.e-3, 0.6*MSOLAR, 1.0*MSOLAR, 4)
+    fisherMatrix(t_obs, calculateAmplitude(10, t_obs), 1.5, freq0, fD_1PN, fDD_1PN)
+
+main(10.e-3, 0.6*MSOLAR, 1.0*MSOLAR, 10*KPCSEC, 4*SECSYEAR)
