25dd26c6d0735ed375f7716dbe998a2d6c3cdb67
gfiacco12 <gfiacco12@gmail.com>
2023-08-10 13:02:38 -0600

    amp calculations and SNR calc

--- graces_phd/Frequency Calculations/main.py
+++ graces_phd/Frequency Calculations/main.py
@@ -1,20 +1,26 @@
 import numpy as np
 from FrequencyCalculations import Frequency_1PN, Frequency_Tides, setupNewtonRaphson
-from FisherCalculations import getFisherMatrix, calculateAmplitude, fisher
+from FisherCalculations import getFisherMatrix, fisher, getSNR
+from HelperCalculations import calculateAmplitude, calculateAmplitude_phys
 from const import *
 
 def main(freq0, mass1, mass2, dl, t_obs):
     #Calculates frequency derivatives and other relevant quantities for binary white dwarf in LISA
     #INPUTS: Initial frequency (Hz), chirp mass (s), total mass (s)
 
+    #frequency calculations
     fD_1PN, fDD_1PN = Frequency_1PN(freq0, mass1, mass2, dl, t_obs)
     fD_tide, fDD_tide = Frequency_Tides(freq0, mass1, mass2, dl, t_obs)
     
-    
-    setupNewtonRaphson(freq0, 0.6*MSOLAR, 0.6*MSOLAR)
+    #setupNewtonRaphson(freq0, 0.6*MSOLAR, 0.6*MSOLAR)
 
-    
+    #Some amplitude/SNR calculations
+    amp = calculateAmplitude(100, t_obs)
+    amp_phys = calculateAmplitude_phys(dl, 0.522*MSOLAR, freq0)
+    snr = getSNR(t_obs, amp_phys, 1.5, freq0, fD_1PN, fDD_1PN)
+    print("SNR:", snr)
+
     #getFisherMatrix(t_obs, calculateAmplitude(10, t_obs), 1.5, freq0, fD_1PN, fDD_1PN)
     #fisher(t_obs, calculateAmplitude(10, t_obs), 1.5, freq0, fD_1PN, fDD_1PN, 1)
 
-main(10.e-3, 0.6*MSOLAR, 1.0*MSOLAR, 10*KPCSEC, 4*SECSYEAR)
+main(10.e-3, 0.6*MSOLAR, 1.0*MSOLAR, 5.6e-20*KPCSEC, 4*SECSYEAR)
